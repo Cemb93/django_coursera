65f5309cbedba10b6d81e636e860acad00e2a67d
Cemb93 <karloz13m@gmail.com>
2025-04-01 00:46:48 -0500

    subiendo proyecto de sourcera

---
+++ myproject/myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Menu(models.Model):
+  name = models.CharField(max_length=100)
+  cuisine = models.CharField(max_length=100)
+  price = models.IntegerField()
+
+class Bebidas(models.Model):
+  bebida = models.CharField(max_length=200)
+  precio = models.IntegerField()
