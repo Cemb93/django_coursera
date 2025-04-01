7ea1f546f21a793ec0452db343aad98b1550516a
Cemb93 <karloz13m@gmail.com>
2025-04-01 02:13:31 -0500

    uso de claves foraneas en modelos

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Bebidas
+from .models import DrinksCategory
 
 # Register your models here.
 admin.site.register(Bebidas)
+admin.site.register(DrinksCategory)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -6,6 +6,10 @@ class Menu(models.Model):
   cuisine = models.CharField(max_length=100)
   price = models.IntegerField()
 
+class DrinksCategory(models.Model):
+  category_name = models.CharField(max_length = 200)
+
 class Bebidas(models.Model):
   bebida = models.CharField(max_length=200)
   precio = models.IntegerField()
+  category_id = models.ForeignKey(DrinksCategory, on_delete=models.PROTECT, default=None)
