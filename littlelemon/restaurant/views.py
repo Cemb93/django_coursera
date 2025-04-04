61da5b3fa9b3d3f3517dd9d48f038940debe1a60
Cemb93 <karloz13m@gmail.com>
2025-04-04 16:49:31 -0500

    Creando items menu littlelemon

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -26,3 +26,14 @@ def menu(request):
     'menu.html',
     {'menu': Menu.objects.all()}
   )
+
+def display_menu_item(request, pk=None):
+  if pk:
+    menu_item = Menu.objects.get(pk=pk)
+  else:
+    menu_item = ''
+  return render(
+    request,
+    'menu_item.html',
+    {'menu_item': menu_item}
+  )
